2fdce80b60ec532db3dd0655b731334b0022707c
JonathanMKPT <jonathan.thomas251@gmail.com>
2020-03-20 21:38:52 -0400

    Completed array review

---
+++ array.php
@@ -0,0 +1,7 @@
+<php>
+	$post_titles = [
+		'10 Things You Always Wanted to Know About Coffee',
+		'8 Worthless Facts About Coffee',
+		'5 Things That Would Matter Except You Need More Coffee'
+	];
+</php>
