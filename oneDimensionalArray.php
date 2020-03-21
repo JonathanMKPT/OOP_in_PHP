64f65599984b78f725ce1066704d087acd90e412
JonathanMKPT <jonathan.thomas251@gmail.com>
2020-03-20 21:39:49 -0400

    Completed one dimensional array

---
+++ oneDimensionalArray.php
@@ -0,0 +1,19 @@
+<php>
+	$posts = [
+		'drinks' => [
+			'coffee' => [
+				'10 Things You Always Wanted To Know About Coffee',
+				'8 Worthless Facts About Coffee',
+				'5 Things That Would Matter Except You Need More Coffee'
+			],
+			'tea' => [
+				'Tea and Other Alternative Caffeine Delivery Systems?',
+				'Decaf Tea: As Opposed To Coffee, This Is A Good Thing',
+			]
+		],
+		'foods' => [
+			'9 Tasty Vegan BBQ Solutions',
+			'5 Sandwiches of Epic Victory'
+		]
+	];
+</php>
